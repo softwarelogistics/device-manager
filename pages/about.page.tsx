a30d78b355c0e3cfb20c1448f9146e219f15b5bf
Kevin D. Wolf <kevinw@software-logistics.com>
2024-12-21 11:24:50 -0500

    about page updates

--- pages/about.page.tsx
+++ pages/about.page.tsx
@@ -14,8 +14,6 @@ import { View } from 'react-native';
 
 export const AboutPage = ({ props, navigation, route }: IReactPageServices) => {
 
-  let version = JSON.stringify(require("../package.json").version).replace('"', '').replace('"', '');
-
   const themePalette = AppServices.instance.getAppTheme();
   const onFetchUpdateAsync = async () => {
     console.log('hi there');
@@ -36,17 +34,16 @@ export const AboutPage = ({ props, navigation, route }: IReactPageServices) => {
   }
 
   return <Page style={[styles.container,  { backgroundColor: themePalette.background }]}>
-      <View style={[styles.stdPadding]}>
-      <AppLogo />
-        <Paragraph content={StaticContent.appDescription}></Paragraph>
-
-        <IconButton color={themePalette.buttonPrimaryText} label={StaticContent.checkForUpdates} icon="cloud-download-outline" iconType="ion" onPress={() => onFetchUpdateAsync()} ></IconButton>
+          <View style={[styles.stdPadding]}>
+            <AppLogo />
+            <Paragraph content={StaticContent.appDescription}></Paragraph>
+            <IconButton color={themePalette.buttonPrimaryText} label={StaticContent.checkForUpdates} icon="cloud-download-outline" iconType="ion" onPress={() => onFetchUpdateAsync()} ></IconButton>
         
-        <WebLink url="https://www.software-logistics.com" label="Software Logistics, LLC"  />
-        <WebLink url="https://www.nuviot.com" label="NuvIoT"  />
-        <WebLink url="https://app.termly.io/document/terms-of-use-for-saas/90eaf71a-610a-435e-95b1-c94b808f8aca" label="Terms and Conditions"  />
-        <WebLink url="https://app.termly.io/document/privacy-policy/fb547f70-fe4e-43d6-9a28-15d403e4c720" label="Privacy Statement"  />          
-        <AppVersionLabel />
-        </View>
+            <WebLink url="https://www.software-logistics.com" label="Software Logistics, LLC"  />
+            <WebLink url="https://www.nuviot.com" label="NuvIoT"  />
+            <WebLink url="https://app.termly.io/document/terms-of-use-for-saas/90eaf71a-610a-435e-95b1-c94b808f8aca" label="Terms and Conditions"  />
+            <WebLink url="https://app.termly.io/document/privacy-policy/fb547f70-fe4e-43d6-9a28-15d403e4c720" label="Privacy Statement"  />          
+            <AppVersionLabel />
+          </View>
         </Page>;
 }
